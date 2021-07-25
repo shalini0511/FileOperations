0e4f88d36200d96411a5fbd3b4e54fc19c25c0c9
shalini <shalini.v@tvsnext.io>
2021-07-25 18:43:43 +0530

    [shalini]Read write and copy text

---
+++ FileIOOperation/FileOperations.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileIOOperation
+{
+    class FileOperations
+    {
+
+        /// <summary>
+        /// file exists or not
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static bool FilExists(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("File exists");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("File not exists");
+                    return false;
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        /// <summary>
+        /// Read all lines in a file - ReadAllLines returns an array of string 
+        /// </summary>
+        public static void ReadAllLinesInFile()
+        {
+            try
+            {
+                string path = @"C:\Users\HP\source\repos\FileIOOperation\FileText.txt";
+                FilExists(path);
+                string[] lines;
+                lines = File.ReadAllLines(path);
+                if (lines.Length != 0)
+                {
+                    foreach (var i in lines)
+                    {
+                        Console.WriteLine(i + " ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File does not contain text");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        /// <summary>
+        /// ReadAllText - returns a single string containing all the lines of the file
+        /// </summary>
+        public static void ReadAllTextInFile()
+        {
+            try
+            {
+                string path = @"C:\Users\HP\source\repos\FileIOOperation\FileText.txt";
+                FilExists(path);
+                string text;
+                text = File.ReadAllText(path);
+                if (text.Length != 0)
+                {
+                    Console.WriteLine(text);
+                }
+                else
+                {
+                    Console.WriteLine("File does not contain text");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Copy File-If third parameter is true, the Copy method will overwrite if file already exists.
+        /// </summary>
+        public static void CopyFile()
+        {
+            string sourcePath = @"C:\Users\HP\source\repos\FileIOOperation\FileText.txt";
+            string destPath = @"C:\Users\HP\source\repos\FileIOOperation\FileText1.txt";
+            try
+            {
+                File.Copy(sourcePath, destPath, true);
+                Console.WriteLine("file is copied successfully");
+            }
+            catch (IOException iox)
+            {
+                Console.WriteLine(iox.Message);
+            }
+        }
+    }
+}
---
+++ FileIOOperation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FileIOOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File IO Operations");
+            string path = @"C:\Users\Radhika\source\repos\FileIOOperations\FileIOOperations\FileText.txt";
+            Console.WriteLine("Enter 1: To check whether the file exists or not");
+            Console.WriteLine("Enter 2: To Read all lines in file by using ReadAllLines method");
+            Console.WriteLine("Enter 3: To Read all lines in file by using ReadAllText method");
+            Console.WriteLine("Enter 4: To copy a file from cource to destination");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    FileOperations.FilExists(path);
+                    break;
+                case 2:
+                    FileOperations.ReadAllLinesInFile();
+                    break;
+                case 3:
+                    FileOperations.ReadAllTextInFile();
+                    break;
+                case 4:
+                    FileOperations.CopyFile();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option!!!");
+                    break;
+            }
+        }
+    }
+}
